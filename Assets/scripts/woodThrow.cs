2e7837cc0ea4abbad226cb0b7d58bba582436709
sakhtopsecret <kulaginsergey09@gmail.com>
2022-05-07 15:17:37 +1100

    Бросок бревна по нажатию и удаление про соприкосновении с поленом в камине

---
+++ Assets/scripts/woodThrow.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class woodThrow : MonoBehaviour {
+public GameObject woodKick;
+private bool touch = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonUp(0))
+        {
+        touch = true;
+        Debug.Log("Pressed primary button.");
+        }
+    }
+    void FixedUpdate()
+    {
+          if(touch == true)
+          {
+          Transform woodKickMove = Instantiate(woodKick.transform, gameObject.transform.position, Quaternion.identity);
+          woodKickMove.GetComponent<Rigidbody>().AddForce(transform.forward *500);
+          touch = false;
+          }
+    }
+}
